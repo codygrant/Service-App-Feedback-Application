9d0822670ffe7b9f1d69028d66f1c26dfa31f2e5
Cody Grant <codygrant0324@gmail.com>
2018-04-30 16:28:08 -0400

    Setup mongoose connection

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1/mern-stack-app');
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 const app = express();
 
