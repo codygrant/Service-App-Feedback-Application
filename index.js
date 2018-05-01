3f9ea3fac9cce094b08337944e5a5b2e5453aa98
Cody Grant <codygrant0324@gmail.com>
2018-05-01 17:49:57 -0400

    Set up cookie-session objects

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
@@ -10,6 +13,16 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 const app = express();
+
+// cookies
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // add express object to routes
 require('./routes/authRoutes')(app);
