f1f9030bc3b11c3eba7c49377a236d817b909000
Cody Grant <codygrant0324@gmail.com>
2018-06-04 12:09:24 -0400

    Create function to grab user data after Stripe payment and implement into button

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 import { FETCH_USER } from "./types";
 
 // create function so redux-thunk will call dispatch function
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get('/api/current_user');
+  const res = await axios.get("/api/current_user");
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+// used to grab updated tokens after payment
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
